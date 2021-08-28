4a0be7c4ea530c2a2e57a45a063da49a985ec467
Daryl <a2ldaryl@gmail.com>
2021-08-28 15:33:25 +0800

    initial

---
+++ js/store.js
@@ -0,0 +1,17 @@
+state: {
+    products: [
+        {
+            id: 1,
+            name: "Classic",
+            image: "",
+            price: 19.90,
+        },
+        {
+            id: 2,
+            name: "Matte Black",
+            image: "",
+            price: 24.90,
+        },
+    ],
+    StoreCart: [],
+},
